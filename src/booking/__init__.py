9e3515a4e0d4983840999c3d5192aecc604a11d6
opbonachera <obonachera@leafnoise.io>
2025-12-23 11:53:41 -0300

    chore: add more error codes and descriptions

--- src/booking/__init__.py
+++ src/booking/__init__.py
@@ -9,9 +9,13 @@ __version__ = "0.1.0"
     FILE_ERROR,
     DB_READ_ERROR,
     DB_WRITE_ERROR,
+    DB_INIT_ERROR,
     JSON_ERROR,
     ID_ERROR,
-) = range(7)
+    DUPLICATED_ROOM_NAME, 
+    ERROR_ELEMENT_NOT_FOUND,
+    DEFAULT
+) = range(11)
 
 ERRORS = {
     DIR_ERROR: "config directory error",
@@ -19,4 +23,8 @@ ERRORS = {
     DB_READ_ERROR: "database read error",
     DB_WRITE_ERROR: "database write error",
     ID_ERROR: "to-do id error",
+    ERROR_ELEMENT_NOT_FOUND: "element to edit was not found",
+    DUPLICATED_ROOM_NAME: "a room with the same name already exists",
+    DB_INIT_ERROR: "error initializing database in specified path. using default",
+    DEFAULT:"",
 }
